1c5267600962855815e134820186f2f90a564905
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2016-09-17 21:26:20 +0200

    Stay on bano-57 for testing purpose

--- builder-cmd.sh
+++ builder-cmd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /data && wget -N http://bano.openstreetmap.fr/data/full.sjson.gz
-gunzip -c full.sjson.gz | addok batch
+cd /data && wget -N http://bano.openstreetmap.fr/data/bano-57.json.gz
+gunzip -c bano-57.json.gz | addok batch
 redis-cli -h redis shutdown save
